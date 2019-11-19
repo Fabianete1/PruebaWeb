ef5d19b1ec9e86126dda22c7b95e6bda8539def2
fabian <fa.villagra@alumnos.duoc.cl>
2019-11-19 08:20:54 -0300

    ultima subida eterna, final boss antes de morir

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls import include
+
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('anime.urls')),    
-  
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('', include('listaanime.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,{
+            'document_root':settings.MEDIA_ROOT,
+        }),
+    ]
